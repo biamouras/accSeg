ace370cece90470eadd2278d0577593cbfc02593
biamouras <biamouras@gmail.com>
2019-10-23 11:00:31 -0300

    adding artificial dataset for segreg

---
+++ R/data.R
@@ -0,0 +1,54 @@
+#' An artificial dataset of spatial distribution of population - case A.
+#'
+#' @source Feitosa, Camara, Monteiro, Koschitzi & Silva (2007). 
+#'    Global and local spatial indices of urban segregation. 
+#'    \emph{International Journal of Geographical Information Science}, 21(3), 299-323.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{id}{A value of the id of the locality.}
+#'  \item{blue}{A value of population of group blue.}
+#'  \item{green}{A value of population of group green.}
+#'  \item{red}{A value of population of group red.}
+#'  \item{yellow}{A value of population of group yellow.}
+#' }
+#' @examples
+#' \dontrun{
+#'  pop_A
+#' }
+"pop_A"
+
+#' An artificial dataset of spatial distribution of population - case B.
+#'
+#' @source Feitosa, Camara, Monteiro, Koschitzi & Silva (2007). 
+#'    Global and local spatial indices of urban segregation. 
+#'    \emph{International Journal of Geographical Information Science}, 21(3), 299-323.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{id}{A value of the id of the locality.}
+#'  \item{blue}{A value of population of group blue.}
+#'  \item{green}{A value of population of group green.}
+#'  \item{red}{A value of population of group red.}
+#'  \item{yellow}{A value of population of group yellow.}
+#' }
+#' @examples
+#' \dontrun{
+#'  pop_B
+#' }
+"pop_B"
+
+#' An artificial dataset of distance matrix between a pair of origin and destination.
+#'
+#' @source Feitosa, Camara, Monteiro, Koschitzi & Silva (2007). 
+#'    Global and local spatial indices of urban segregation. 
+#'    \emph{International Journal of Geographical Information Science}, 21(3), 299-323.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{origin}{A value of the id of the locality origin.}
+#'  \item{destination}{A value of the id of the locality destination.}
+#'  \item{travel_time}{A value of the distance between origin and destination.}
+#' }
+#' @examples
+#' \dontrun{
+#'  matrix
+#' }
+"matrix"
