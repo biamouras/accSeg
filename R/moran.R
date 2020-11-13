53ac7162461fdbcd1d3c8fd66b07bdd1d7f2d457
Beatriz Moura <biamouras@gmail.com>
2020-11-13 13:38:30 -0300

    simplifying localMoran

--- R/moran.R
+++ R/moran.R
@@ -260,36 +260,15 @@ LISAmaps <- function(shp=NULL, x=NULL, y=NULL, W=NULL, local_sims=NULL, local_mo
 #'
 #' @return the local values of Moran's I
 
-localMoran <- function(shp=NULL, x=NULL, y = NULL, W=NULL){
+localMoran <- function(x=NULL, y = NULL, W=NULL){
   
-  # if shp is NULL then it must have x and W
-  if(is.null(shp)){
-    # if x is NULL then stop
-    if(is.null(x)) stop('shp and x are NULL. Please add any of these parameters')
-    # if W is NULL then stop
-    if(is.null(W)) stop('shp and W are NULL. Please add any of these parameters')
-    # if y is NULL the process is not bivariate
-    if(is.null(y)) y <- x
-  } else{
-    # if class of shp is sf
-    if(class(shp)[1] == "sf") {
-      shp_sf <- shp
-      shp <- sf::as_Spatial(shp)
-    } else{
-      shp_sf <- sf::st_as_sf(shp)
-    }
-    # process the W
-    W <- nbMatrix(shp)
-    # x
-    nm <- names(shp_sf)
-    x <- shp_sf[[nm[2]]]
-    if(ncol(shp) == 2){
-      y <- x
-    } else if(ncol(shp) == 3){
-      y <- shp_sf[[nm[3]]]
-    } else {stop('shp is bigger or shorter than expected')}
-  }
-  
+  # if x is NULL then stop
+  if(is.null(x)) stop('shp and x are NULL. Please add any of these parameters')
+  # if W is NULL then stop
+  if(is.null(W)) stop('shp and W are NULL. Please add any of these parameters')
+  # if y is NULL the process is not bivariate
+  if(is.null(y)) y <- x
+
   # scaling variables
   xp <- as.numeric(scale.default(x))
   yp <- as.numeric(scale.default(y))
